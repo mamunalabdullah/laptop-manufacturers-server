cdcec06e7f3194c79ed2d0a9930542f9f27e2c32
Abdullah Al Mamun <abdullahmamun1597@gmail.com>
2022-05-25 12:53:04 +0600

    review add

--- index.js
+++ index.js
@@ -1,42 +1,50 @@
-const express = require('express')
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-const app = express()
-const port = process.env.PORT || 5000
+const app = express();
+const port = process.env.PORT || 5000;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nihvi.mongodb.net/?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-async function run(){
-    try{
-        await client.connect();
-        console.log("Database connect");
-        const toolsCollection = client.db("laptop-manufacturers").collection("tools");
-
-      // read all 
-        app.get("/tools", async(req, res) => {
-            const query = {};
-            const cursor = toolsCollection.find(query);
-            const tools = await cursor.toArray();
-            res.send(tools);
-        })
-
-        // find one 
-        app.get('/tool/:id', async (req, res) => {
-          const id = req.params.id;
-          const filter = { _id: ObjectId(id) };
-          const tool = await toolsCollection.findOne(filter);
-          res.send(tool);
+async function run() {
+  try {
+      await client.connect();
+      const toolsCollection = client.db("laptop-manufacturers").collection("tools")
+      const reviewsCollection = client.db("laptop-manufacturers").collection("reviews")
+      // Read all data
+      app.get('/tools', async (req, res) => {
+          const query = req.query;
+          const cursor = toolsCollection.find(query);
+          const result = await cursor.toArray()
+          // console.log(result);
+          res.send(result);
       })
 
-        // Update one
-        app.put('/update/:id', async (req, res) => {
+      // Find a data
+
+      app.get('/tool/:id', async (req, res) => {
+          const id = req.params.id;
+          const filter = { _id: ObjectId(id)
+};
+          const result = await toolsCollection.findOne(filter);
+          res.send(result);
+      })
+
+      // Update a data
+
+      app.put('/update/:id', async (req, res) => {
           const id = req.params.id;
           const data = req.body;
-          const filter = { _id: ObjectId(id) };
+          const filter = { _id: ObjectId(id)
+};
           const options = { upsert: true };
           const updateDoc = {
               $set: {
@@ -46,21 +54,41 @@ async function run(){
           const result = await toolsCollection.updateOne(filter, updateDoc, options);
           res.send(result);
       })
-      }
-    finally{
-        // await client.close();
-    }
+
+      // Review part
+      // Review all data
+      app.get('/review', async (req, res) => {
+          const query = req.query;
+          const cursor = reviewsCollection.find(query);
+          const result = await cursor.toArray()
+          // console.log(result);
+          res.send(result);
+      })
+
+      //Create a review
+
+      app.post('/review', async (req, res) => {
+
+          const data = req.body;
+
+          const result = await reviewsCollection.insertOne(data);
+          res.send(result);
+      })
+
+
+  } finally {
+
+  }
 }
 run().catch(console.dir);
-
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello Laptop-Manufacturers!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello Laptop-Manufacturers!");
+});
 
 app.listen(port, () => {
-  console.log(`Laptop-Manufacturers app listening on port ${port}`)
-})
+  console.log(`Laptop-Manufacturers app listening on port ${port}`);
+});
